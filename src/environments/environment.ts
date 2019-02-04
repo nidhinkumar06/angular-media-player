873ea9d241457ade0cc0c1874c9641cfd1a62583
nidhinkumar06 <nidhinkumar06@gmail.com>
2019-02-04 13:37:39 +0530

    Remove credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBpOX2ZwtnVVKan8j-yxDwvE3q4JwLCek0",
-    authDomain: "reactflashcards-45cbc.firebaseapp.com",
-    databaseURL: "https://reactflashcards-45cbc.firebaseio.com",
-    projectId: "reactflashcards-45cbc",
-    storageBucket: "reactflashcards-45cbc.appspot.com",
-    messagingSenderId: "391026593864"
   }
 };
 
