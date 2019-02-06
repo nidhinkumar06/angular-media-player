74d58d5662a6ec0167585f8fc7beabc0572768cd
nidhinkumar <nidhinkumar@codingtown.com>
2019-02-06 13:32:15 +0530

    Add production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    
+    apiKey: "AIzaSyBpOX2ZwtnVVKan8j-yxDwvE3q4JwLCek0",
+    authDomain: "reactflashcards-45cbc.firebaseapp.com",
+    databaseURL: "https://reactflashcards-45cbc.firebaseio.com",
+    projectId: "reactflashcards-45cbc",
+    storageBucket: "reactflashcards-45cbc.appspot.com",
+    messagingSenderId: "391026593864"
   }
 };
